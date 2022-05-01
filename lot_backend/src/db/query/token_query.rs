dff28060e13c334eaa26ee28e1bd745610840230
sungwooHa <ttlhi10@gmail.com>
2022-05-02 00:44:51 +0900

    user_token update query 추가

--- lot_backend/src/db/query/token_query.rs
+++ lot_backend/src/db/query/token_query.rs
@@ -10,3 +10,9 @@ pub fn get_user_by_uuid(conn: &MysqlConnection, _uuid: &i64) -> QueryResult<User
         .filter(uuid.eq(_uuid))
         .get_result::<UserToken>(&*conn)
 }
+
+pub fn update_user_token(conn: &MysqlConnection, user_token: &UserToken) -> QueryResult<usize> {
+    diesel::update(tbl_user_token.find(user_token.uuid))
+        .set(user_token)
+        .execute(&*conn)
+}
